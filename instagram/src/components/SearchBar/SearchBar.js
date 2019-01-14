0508aaf426c90af0d95b561b75e9d40e75716c20
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-14 15:42:47 -0600

    Added icons to the search bar from FontAwesome and imported it in the index.html file

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -3,8 +3,13 @@ import React from 'react';
 function SearchBar(props){
     return (
         <header>
+            <i class="fas fa-camera"></i>
+            <div className="line"/>
             <h1 className="logo">Instagram</h1>
             <input className="searchBar" placeholder="Search"/>
+            <i class="far fa-compass"></i>
+            <i class="far fa-heart"></i>
+            <i class="far fa-user"></i>
         </header>
     );
 }
