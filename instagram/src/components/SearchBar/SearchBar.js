2a9186775f8a067ced27171b07a586c1a59b9310
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-14 15:36:07 -0600

    Imported the SearchBar file to the main app.js file and rendered it along side the postcontainer with some basic info

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchBar(props){
+    return (
+        <header>
+            <h1 className="logo">Instagram</h1>
+            <input className="searchBar" placeholder="Search"/>
+        </header>
+    );
+}
+
+export default SearchBar;
