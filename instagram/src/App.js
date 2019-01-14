29eff440576d8df74fbea7f1d96ba8671918a28a
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-14 14:54:24 -0600

    Set up the basics for the App.js file to pass down props to the postContainer file. Then set up the postContainer file to pass down props to the CommentSection file

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import dummyData from './dummy-data';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      dataArray: dummyData
+    };
+  }
+  
   render() {
     return (
       <div className="App">
-        
+        <PostContainer postData={this.dataArray}/>
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CommentSection from './CommentSection/CommentSection.js';
+
+function PostContainer(props){
+    return (
+        <div className="posts-container">
+            {props.postData.map((post, index) => {
+                return <CommentSection post={post} key={index}/>
+            })};
+        </div>
+    );
+}
+
+export default PostContainer;
