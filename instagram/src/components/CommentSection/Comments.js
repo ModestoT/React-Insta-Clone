394e4b75146e1b89662e813ff2fc3e8c6dae211b
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-15 16:01:29 -0600

    Added functionality to be able to add a comment to a post

--- instagram/src/components/CommentSection/Comments.js
+++ instagram/src/components/CommentSection/Comments.js
@@ -5,14 +5,29 @@ class Comments extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            currentComments: props.comments,
+            comments: props.comments,
             comment: ''
         };
     }
+
+    handleInput = e => {
+        this.setState({ comment: e.target.value });
+    }
+
+    addNewComment = (e) => {
+        e.preventDefault();
+        const newComment = {text: this.state.comment, username: "BillyGachi"};
+        const comments = this.state.comments.slice();
+
+        comments.push(newComment);
+
+        this.setState({comments: comments, comment: ''});
+    }
+
     render(){
         return (
             <div>
-                {this.state.currentComments.map((comment, index) => {
+                {this.state.comments.map((comment, index) => {
                     return (
                         <div key ={index} className="comments">
                             <h2 className="comment-username">{comment.username}</h2>
@@ -21,11 +36,13 @@ class Comments extends React.Component {
                     )
                 })}
                 <p className="timestamp">{Moment().startOf('day').fromNow().toUpperCase()}</p>
-                <input
-                    className="comment-box"
-                    placeholder="Add a comment..."
-                    
-                    onClick={(e) => console.log(e.target)}/>
+                <form onSubmit={this.addNewComment}>
+                    <input
+                        className="comment-box"
+                        placeholder="Add a comment..."
+                        value = {this.state.comment}
+                        onChange={this.handleInput}/>
+                </form>
             </div>
         );
     }
