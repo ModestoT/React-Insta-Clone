81e523f10bca1f9f8b840bb4e6a587191e7e08c5
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-17 11:59:20 -0600

    Refactored code to be more inline with the ReadMe as well as imported styled-components

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import dummyData from './dummy-data';
+
 import PostPage from './components/PostContainer/PostPage';
 import authenticate from './authentication/authenticate';
 import Login from './components/Login/Login';
@@ -11,47 +11,20 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      dataArray: [],
-      filterPost: ''
+      
     };
   }
 
-  handleSearch = e => {
-    this.setState({ filterPost: e.target.value, 
-    }, () => {
-      this.filterPost();
-    });
-  }
-
-  filterPost = () => {
-    const currentList = document.querySelectorAll('.post-container');
-    const newList = Array.from(currentList);
-    
-    newList.forEach(post => {
-      if(post.firstChild.textContent.indexOf(this.state.filterPost) === -1){
-        post.classList.add('hide');
-      } else {
-        post.classList.remove('hide');
-      }
-    })
-  }
-
-
-  componentDidMount() {
-    this.setState({dataArray: dummyData});
-
-  }
   
   render() {
     return (
       <div className="App">
-        {/* <SearchBar handleSearch={this.handleSearch} filterPost={this.state.filterPost}/>
-        <PostContainer postData={this.state.dataArray} /> */}
-        {/* <Login handleLogin={this.handleLogin} login={this.login}/> */}
-        <PostPage handleSearch={this.handleSearch} filterPost={this.state.filterPost} postData={this.state.dataArray}/>
+        <ConditionalView />
       </div>
     );
   }
 }
 
-export default authenticate(App)(Login);
+const ConditionalView = authenticate(PostPage)(Login);
+
+export default App;
--- instagram/src/components/PostContainer/PostPage.js
+++ instagram/src/components/PostContainer/PostPage.js
@@ -1,12 +1,50 @@
 import React from 'react';
+
+import dummyData from '../../dummy-data';
 import SearchBar from '../SearchBar/SearchBar';
 import PostContainer from './PostContainer';
 
-const PostPage = props => {
-    return <div>
-            <SearchBar handleSearch={props.handleSearch} filterPost={props.filterPost}/>
-            <PostContainer postData={props.postData} />
+class PostPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            dataArray: [],
+            filterPost: ''
+        };
+    }
+    
+  handleSearch = e => {
+    this.setState({ filterPost: e.target.value, 
+    }, () => {
+      this.filterPost();
+    });
+  }
+
+  filterPost = () => {
+    const currentList = document.querySelectorAll('.post-container');
+    const newList = Array.from(currentList);
+    
+    newList.forEach(post => {
+      if(post.firstChild.textContent.indexOf(this.state.filterPost) === -1){
+        post.classList.add('hide');
+      } else {
+        post.classList.remove('hide');
+      }
+    })
+  }
+
+
+  componentDidMount() {
+    this.setState({dataArray: dummyData});
+
+  }
+
+    render() {
+        return <div>
+            <SearchBar handleSearch={this.handleSearch} filterPost={this.state.filterPost}/>
+            <PostContainer postData={this.state.dataArray} />
         </div>
+    }
 };
 
 export default PostPage;
