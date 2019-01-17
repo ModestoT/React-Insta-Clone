4b3465e5412426d5c576f76358cd7959cb021af4
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-17 15:57:13 -0600

    Refactored the Login page to use styled components instead of CSS

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,6 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './Login.css';
+
+const LoginContainer = styled.div `
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const LoginForm = styled.form `
+    border: 1px solid darkgrey;
+    width: 27%;
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10%;
+`;
+const LoginHeader = styled.h1 `
+    font-size: 2.8rem;
+    font-family: 'Lobster', cursive;
+`;
+
+const LoginP = styled.p `
+    font-family: Arial, sans-serif;
+    margin: 5%;
+`;
+
+const LoginInput = styled.input `
+    width: 50%;
+    background: #FAFAFA;
+    border: 1px solid lightgrey;
+    border-radius: 3px;
+    padding: 2%;
+    margin-bottom: 6%;
+`;
+
+const LoginButton = styled.button `
+    width: 50%;
+    padding: 1%;
+    background: dodgerblue;
+    border: 1px solid dodgerblue;
+    border-radius: 3px;
+    color: white;
+`;
 
 class Login extends React.Component {
     constructor(props){
@@ -27,25 +74,25 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div className="login-container">
-                <form className="login-form">
-                    <h1>Insta Clone</h1>
-                    <p>Login and see photos from your friends!</p>
-                    <input 
+            <LoginContainer>
+                <LoginForm>
+                    <LoginHeader>Insta Clone</LoginHeader>
+                    <LoginP>Login and see photos from your friends!</LoginP>
+                    <LoginInput 
                         type="text"
                         name="username"
                         placeholder="Username"
                         value={this.state.username}
                         onChange={this.handleLogin}/>
-                    <input 
+                    <LoginInput 
                         type="password"
                         name="password"
                         placeholder="Password"
                         value={this.state.password}
                         onChange={this.handleLogin}/>
-                    <button onClick={this.login}>Login</button>
-                </form>
-            </div>
+                    <LoginButton onClick={this.login}>Login</LoginButton>
+                </LoginForm>
+            </LoginContainer>
         );
     }
 }
